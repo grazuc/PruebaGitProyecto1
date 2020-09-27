c276ca4961d188d45c927ef6916895e409bae709
Gonza Csb <gonzalorazuc@gmail.com>
2020-09-26 21:52:09 -0300

    Tercer commit

--- Persona.java
+++ Persona.java
@@ -1,6 +1,6 @@
 public class Persona{
 	public static void main(String [] args){
-		Persona p = new Persona("Jorge");
+		Persona p = new Persona("Esteban");
 		System.out.println("nombre: " + p.getName());
 	}
 }
