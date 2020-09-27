da332147eb19030588559f960ebbf0b97ca1458f
Gonza Csb <gonzalorazuc@gmail.com>
2020-09-26 21:46:27 -0300

    Primer commit

---
+++ Auto.java
@@ -0,0 +1,6 @@
+public class Auto{
+	public static void main(String [] args){
+		Auto p = new Auto("Ford Fiesta");
+		System.out.println("nombre: " + p.getAuto());
+	}
+}  
---
+++ Persona.java
@@ -0,0 +1,6 @@
+public class Persona{
+	public static void main(String [] args){
+		Persona p = new Persona("Jorge");
+		System.out.println("nombre: " + p.getName());
+	}
+}
